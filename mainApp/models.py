2c9252756b18b63aee3a52711491ea6b3241b2c9
Christian Sumares <christiansumares@gmail.com>
2019-11-02 16:15:14 -0700

    Added __str__ methods to models

--- mainApp/models.py
+++ mainApp/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 class Neighborhood(models.Model):
 	name = models.CharField(blank=False, max_length=1000)
+	def __str__(self):
+		return self.name
 
 class Location(models.Model):
 	street_name = models.CharField(blank=False, max_length=1000)
 	neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.street_name
 
 class Issue(models.Model):
 	issue_type = models.CharField(blank=False, max_length=1000)
@@ -13,6 +17,8 @@ class Issue(models.Model):
 	location = models.ForeignKey(Location, on_delete=models.CASCADE)
 	avg_dur = models.IntegerField(null=True)
 	date_modified = models.DateField()
+	def __str__(self):
+		return self.issue_type + " on " + self.location.street_name
 
 class Date(models.Model):
 	date = models.DateField(blank=False)
