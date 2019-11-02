26ad3008466cbd394ea4d3f085ec2936478b6525
Christian Sumares <christiansumares@gmail.com>
2019-11-02 13:10:36 -0700

    added utils.py and models

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+	street_name = models.CharField(blank=False, max_length=1000)
+
+class Issue(models.Model):
+	issue_type = models.CharField(blank=False, max_length=1000)
+	next_date = models.DateField(null=True)
+	location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+class Date(models.Model):
+	date = models.DateField(blank=False)
+	issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
---
+++ mainApp/utils.py
@@ -0,0 +1,25 @@
+from datetime import date, timedelta
+
+# PARAMS: list of dates, date_list
+# RETURNS: timedelta, the average duration between each date
+def avg_duration(date_list):
+	if len(date_list) < 2:
+		return 0
+	durationCount = 1
+	durationSum = date_list[1] - date_list[0]
+	for i in range(2, len(date_list)):
+		durationSum += (date_list[i] - date_list[i-1])
+		durationCount += 1
+	return (durationSum / durationCount)
+
+# PARAMS: list of dates, date_list
+# RETURNS: date next_date, an estimation of when something might attention
+def predict_next(date_list):
+	avg_dur = avg_duration(date_list)
+	next_date = date_list[len(date_list)-1]+avg_dur
+	return next_date
+
+# PARAMS: date next_date, timedelta margin
+# RETURNS: list of 2 dates, beginning and end
+def guess_duration(next_date, margin):
+	return [next_date - (margin / 2), next_date + (margin / 2)]
