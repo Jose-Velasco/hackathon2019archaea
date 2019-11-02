17af0948e39161de77cd395401a2bba34ad9b31e
Christian Sumares <christiansumares@gmail.com>
2019-11-02 15:15:00 -0700

    expanded utils, more models

--- mainApp/models.py
+++ mainApp/models.py
@@ -5,12 +5,22 @@ class Neighborhood(models.Model):
 
 class Location(models.Model):
 	street_name = models.CharField(blank=False, max_length=1000)
+	neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
 
 class Issue(models.Model):
 	issue_type = models.CharField(blank=False, max_length=1000)
 	next_date = models.DateField(null=True)
 	location = models.ForeignKey(Location, on_delete=models.CASCADE)
+	avg_dur = models.IntegerField(null=True)
+	date_modified = models.DateField()
 
 class Date(models.Model):
 	date = models.DateField(blank=False)
 	issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+
+class Alert(models.Model):
+	enabled = models.BooleanField(default=True)
+	issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+	message = models.CharField(blank=False, max_length=1000)
+	start_date = models.DateField()
+	end_date = models.DateField()
--- mainApp/utils.py
+++ mainApp/utils.py
@@ -1,7 +1,64 @@
 from datetime import date, timedelta
+from .models import Neighborhood, Location, Date
+#from django.core.mail import send_mail
 
-def generate_report():
+def send_emails():
 	pass
+
+def check_for_new_alerts():
+	neighborhoods = Neighborhood.objects.all()
+	for n in neighborhoods:
+		locations = n.location_set.all()
+		for loc in locations:
+			issues = loc.issue_set.all()
+			for iss in issues:
+				# check if there is already an existing alert about this issue
+				if alert_exists(iss.id):
+					continue
+				duration = guess_duration(iss.next_date, 10)
+				today = date.today()
+				# Otherwise, create an alert if needed
+				if today >= duration[0] and today <= duration[1]:
+					generate_alert(iss.id, duration[0], duration[1])
+
+# PARAMS: int alert_id, the id of the alert to disable
+# RETURNS: NOTHING
+def disable_alert(alert_id):
+	if Alert.objects.filter(id=alert_id).exists():
+		alert = Alert.objects.get(id=alert_id)
+		alert.enabled = False
+		alert.save()
+
+def generate_alert(issue_id, start_day, end_day):
+	iss = Issue.objects.get(id=issue_id)
+	street_name = iss.location.street_name
+	n_name = iss.location.neighborhood.name
+	# Ex: 15TH AVE (Inner Richmond) is estimated to need Tree Maintenance attention between Oct 30 2019 and Nov 09 2019
+	msg = street_name
+	    + " ("
+	    + n_name
+	    + ") is estimated to need "
+	    + iss.issue_type
+	    + " attention between "
+	    + start_day.strftime("%b %d %Y")
+	    + " and "
+	    + end_day.strftime("%b %d %Y")
+	new_alert = Alert(
+		issue=iss,
+		message=msg,
+		start_date=start_day,
+		end_date=end_day
+	)
+	new_alert.save()
+
+# PARAMS: int issue_id
+# RETURNS: boolean, if an alert already exists about that issue
+def alert_exists(issue_id):
+	alerts = Alert.objects.all()
+	for alert in alerts:
+		if alert.issue.id == issue_id:
+			return True
+	return False
 
 # PARAMS: list of dates, date_list
 # RETURNS: timedelta, the average duration between each date
